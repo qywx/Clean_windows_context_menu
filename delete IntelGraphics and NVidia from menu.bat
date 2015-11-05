4746cfd2ed84cf27bb478a5e47103b66c61463cc
qyw <i.kyb@ya.ru>
2015-11-05 23:26:49 +0200

    no message

---
+++ delete IntelGraphics and NVidia from menu.bat
@@ -0,0 +1,3 @@
+reg delete "HKEY_CLASSES_ROOT\Directory\Background\shellex\ContextMenuHandlers\igfxcui" /f
+reg delete "HKEY_CLASSES_ROOT\Directory\Background\shellex\ContextMenuHandlers\igfxDTCM" /f
+reg delete "HKEY_CLASSES_ROOT\Directory\Background\shellex\ContextMenuHandlers\NvCplDesktopContext" /f
